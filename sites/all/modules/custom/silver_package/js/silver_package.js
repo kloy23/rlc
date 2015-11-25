5f03b643abb9af6100ff93232812186d8e802afd
Clifton Loy <loyswebdev@gmail.com>
2015-11-25 08:28:10 -0600

    Added and Enabled custom module silver_package.

---
+++ sites/all/modules/custom/silver_package/js/silver_package.js
@@ -0,0 +1,41 @@
+(function($) { // Throughout the document, replace "$" with "jQuery"
+  'use strict';
+  $(document).ready(function() { // Begin $(document).ready()
+
+    var $productSelect = $('.silver_package_product_select input');
+    var $productQuantity = $('.silver_package_product_quantity');
+    var $quantitySelect = $('.silver_package_product_quantity select');
+    $productQuantity.hide();
+
+    var toggleQuantity = function(e) {
+      var $productDiv = $(e.target).parents().eq(2);
+      var $quantity = $productDiv.children().eq(1);
+      $quantity.toggle();
+    };
+    var clearProducts = function() {
+      for (var i = 0; i < $productSelect.length; i++) {
+        var $product = $productSelect[i];
+        $product.checked = false;
+      };
+    };
+    var clearQuantity = function() {
+      for (var i = 0; i < $quantitySelect.length; i++) {
+        var $quantity = $quantitySelect[i];
+        $quantity.value = 0;
+      };
+    };
+
+    $('#addToCart').click(function(e) {
+      // e.preventDefault();
+    });
+
+    $productSelect.click(function(e) {
+      toggleQuantity(e);
+    });
+
+    $(function setDefaults() {
+      clearProducts();
+      clearQuantity();
+    });
+  }); // End $(document).ready()
+})(jQuery); // End jQuery $ replacement
